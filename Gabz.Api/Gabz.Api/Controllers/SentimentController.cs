f3e17efd3aeecc255a858b36a2c17a9d79ac411b
Petar <petar.kovacevic@axilis.com>
2016-04-14 20:57:09 +0200

    Project init

---
+++ Gabz.Api/Gabz.Api/Controllers/SentimentController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Threading.Tasks;
+using Gabz.Api.Models;
+using Gabz.Core.Models;
+using System.Collections.Generic;
+
+namespace Gabz.Api.Controllers
+{
+    public class SentimentController : ApiController
+    {
+
+        public async Task<SentimentResult> Calculate(string document)
+        {
+            return await SentimentService.Analyse(document);
+        }
+
+
+
+    }
+}
---
+++ Gabz.Api/Gabz.Core/Models.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gabz.Core.Models
+{
+
+    public class SentimentResult
+    {
+        public string Sentence { get; set; }
+        public float SentimentScore { get; set; }
+    }
+
+    internal class SentimentInput
+    {
+        public List<Document> Documents { get; set; }
+
+        public SentimentInput()
+        {
+            Documents = new List<Document>();
+        }
+    }
+ 
+    internal class Document
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    internal class SentimentOutput
+    {
+        public List<Result> Documents { get; set; }
+        public SentimentOutput()
+        {
+            Documents = new List<Result>();
+        }
+
+    }
+
+    internal class Result
+    {
+        public int Id { get; set; }
+        public float Score { get; set; }
+
+    }
+
+    
+
+
+}
